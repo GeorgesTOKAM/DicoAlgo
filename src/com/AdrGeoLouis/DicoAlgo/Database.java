8107efb28615b6516344785a2e7eb70627c7d905
GeorgesTOKAM <thierrytokam@gmail.com>
2018-02-16 18:36:53 +0100

    database librairy

--- src/com/AdrGeoLouis/DicoAlgo/Database.java
+++ src/com/AdrGeoLouis/DicoAlgo/Database.java
@@ -1,5 +1,12 @@
 package com.AdrGeoLouis.DicoAlgo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Database {
 	
 	
